124949fcb006e57b0fbfb5c516571226bab05326
marvin <mmende25@calstatela.edu>
2017-09-19 22:30:25 -0700

    updated main to accept user input

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String src = "C:\\Users\\Rich Homie Quan\\SCM\\SCM\\mypt2";
-		String target = "C:\\Users\\Rich Homie Quan\\SCM\\SCM\\mypt3";
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter source tree path: ");
+		String src = kb.nextLine();
+		System.out.println("Enter target repo folder path: ");
+		String target = kb.nextLine();
 		Repository rep = new Repository(src, target);
 		rep.execute();
 	}
